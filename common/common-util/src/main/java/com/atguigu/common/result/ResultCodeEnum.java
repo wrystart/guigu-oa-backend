7bb14dfa76f3a2671a4793ac311d314622a6ea89
Ruiyang Wang <895437987@qq.com>
2023-10-18 15:00:25 +0800

    feat: 统一返回结果状态信息类

---
+++ common/common-util/src/main/java/com/atguigu/common/result/ResultCodeEnum.java
@@ -0,0 +1,31 @@
+package com.atguigu.common.result;
+
+import lombok.Getter;
+
+/**
+ * @Description 统一返回结果状态信息类
+ * @Author wrystart
+ * @Version
+ * @Date 2023/10/18 14:55
+ */
+@Getter
+public enum ResultCodeEnum {
+
+    SUCCESS(200,"成功"),
+    FAIL(201, "失败"),
+    SERVICE_ERROR(2012, "服务异常"),
+    DATA_ERROR(204, "数据异常"),
+
+    LOGIN_AUTH(208, "未登陆"),
+    PERMISSION(209, "没有权限")
+    ;
+
+    private Integer code;
+
+    private String message;
+
+    private ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
