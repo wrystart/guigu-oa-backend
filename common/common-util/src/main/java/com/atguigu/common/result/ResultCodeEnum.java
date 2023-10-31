c3fc081086f3dc27c6ed2a58d2d998a8c48f7d4e
Ruiyang Wang <895437987@qq.com>
2023-10-31 09:34:00 +0800

    feat: 添加用户登录失败状态码

--- common/common-util/src/main/java/com/atguigu/common/result/ResultCodeEnum.java
+++ common/common-util/src/main/java/com/atguigu/common/result/ResultCodeEnum.java
@@ -16,6 +16,8 @@ public enum ResultCodeEnum {
     SERVICE_ERROR(2012, "服务异常"),
     DATA_ERROR(204, "数据异常"),
 
+
+    LOGIN_MOBLE_ERROR(205,"认证失败"),
     LOGIN_AUTH(208, "未登陆"),
     PERMISSION(209, "没有权限")
     ;
