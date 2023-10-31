fcb6aa19c0c8d0b98bf4b5b262e50f0a7127bc0f
Ruiyang Wang <895437987@qq.com>
2023-10-31 09:33:37 +0800

    feat: 新增请求相应工具类

---
+++ common/common-util/src/main/java/com/atguigu/common/result/ResponseUtil.java
@@ -0,0 +1,28 @@
+package com.atguigu.common.result;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description 请求响应工具类
+ * @Author wrystart
+ * @Version 1.0
+ * @Date 2023/10/31 9:07
+ */
+public class ResponseUtil {
+
+    public static void out(HttpServletResponse response, Result r) {
+        ObjectMapper mapper = new ObjectMapper();
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            mapper.writeValue(response.getWriter(), r);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
