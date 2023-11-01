1337fd051d5aa61f5af0d02b1af48c99a3d2b47e
Ruiyang Wang <895437987@qq.com>
2023-11-01 16:22:53 +0800

    feat: 审批类型管理-CURD接口

---
+++ service-oa/src/main/java/com/atguigu/process/controller/OaProcessTypeController.java
@@ -0,0 +1,64 @@
+package com.atguigu.process.controller;
+
+import com.atguigu.common.result.Result;
+import com.atguigu.model.process.ProcessType;
+import com.atguigu.process.service.OaProcessTypeService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Description 审批类型管理
+ * @Author Ruiyang Wang
+ * @Version 1.0
+ * @Date 2023/11/1 16:05
+ */
+@Api(value = "审批类型管理", tags = "审批类型管理")
+@RestController
+@RequestMapping(value = "/admin/process/processType")
+public class OaProcessTypeController {
+
+    @Autowired
+    private OaProcessTypeService oaProcessTypeService;
+
+    @ApiOperation(value = "分页查询")
+    @GetMapping("{page}/{limit}")
+    public Result index(@PathVariable Long page,
+                        @PathVariable Long limit) {
+        Page<ProcessType> pageParam = new Page<>(page,limit);
+        IPage<ProcessType> pageModel = oaProcessTypeService.page(pageParam);
+        return Result.ok(pageModel);
+    }
+
+    @ApiOperation(value = "获取审批类型")
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id) {
+        ProcessType processType = oaProcessTypeService.getById(id);
+        return Result.ok(processType);
+    }
+
+    @ApiOperation(value = "添加审批类型")
+    @PostMapping("save")
+    public Result save(@RequestBody ProcessType processType) {
+        oaProcessTypeService.save(processType);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "修改审批类型")
+    @PutMapping("update")
+    public Result updateById(@RequestBody ProcessType processType) {
+        oaProcessTypeService.updateById(processType);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "删除审批类型")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        oaProcessTypeService.removeById(id);
+        return Result.ok();
+    }
+}
+
---
+++ service-oa/src/main/java/com/atguigu/process/service/impl/OaProcessTypeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.atguigu.process.service.impl;
+
+import com.atguigu.model.process.ProcessType;
+import com.atguigu.process.mapper.OaProcessTypeMapper;
+import com.atguigu.process.service.OaProcessTypeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 审批类型 服务实现类
+ * </p>
+ *
+ * @author Ruiyang Wang
+ * @since 2023-11-01
+ */
+@Service
+public class OaProcessTypeServiceImpl extends ServiceImpl<OaProcessTypeMapper, ProcessType> implements OaProcessTypeService {
+
+}
